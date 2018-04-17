484bda1c6985c887caa1d3d86c93ed96bbc80b56
sg <396857483@qq.com>
2018-04-17 17:53:04 +0800

    fix

--- ws/08/ws.js
+++ ws/08/ws.js
@@ -13,20 +13,25 @@ app.get('/jquery.js', function (req, res) {
   res.sendFile(__dirname + '/jquery.min.js');
 });
 
+app.get('/ws.css', function (req, res) {
+  res.sendFile(__dirname + '/ws.css');
+});
+
 io.on('connection', function (socket) {
   console.log('a user connected');
   socket.on('register', function (data) {
     // 判断用户名是否重复
-    for (var i=0; i<users.length; i++) {
+    for (var i = 0; i < users.length; i++) {
       if (data.username == users[i].username) {
         socket.emit('registerSuccess', 'used');
         return;
       }
     }
     users.push({
-      id: socket.id,  // 用socket提供的ID进行定向推送信息或者其他事件；
+      id: socket.id, // 用socket提供的ID进行定向推送信息或者其他事件；每次刷新浏览器ID会变；
       avatar: '',
       username: data.username,
+      messages: [],
       friends: [],
       groups: [],
       strangers: [],
@@ -38,10 +43,11 @@ io.on('connection', function (socket) {
     // 判断用户名是否存在
     var hasUser = null;
     var num = 0;
-    for (var i=0; i<users.length; i++) {
+    for (var i in users) {
       if (data.username === users[i].username) {
         hasUser = true;
         num = i;
+        users[i].id = socket.id;
         break;
       } else {
         hasUser = false;
@@ -56,11 +62,11 @@ io.on('connection', function (socket) {
     } else {
       socket.emit('loginSuccess', 'none');
     }
-    
+
     //console.log(users)
   });
 
-  socket.on('addFriend', function(data, user){
+  socket.on('addFriend', function (data, user) {
     // 判断所有用户有无此人
     var hasFriend = false;
 
@@ -69,7 +75,7 @@ io.on('connection', function (socket) {
       return;
     }
 
-    for (var i=0; i<users.length; i++) {
+    for (var i in users) {
       if (users[i].username === data) {
         hasFriend = true;
         break;
@@ -80,17 +86,17 @@ io.on('connection', function (socket) {
 
     if (hasFriend) {
       // 判断用户列表有无此好友
-      for (var i=0; i<users[socket.userNum].friends.length; i++) {
-        if (users[socket.userNum].friends[i] === data){
+      for (var i = 0; i < users[socket.userNum].friends.length; i++) {
+        if (users[socket.userNum].friends[i] === data) {
           socket.emit('addFriendSuccess', 'already');
           return;
         }
       }
 
       // 判断对方列表有无此陌生人
-      for (var i=0; i<users[socket.userNum].strangers.length; i++) {
-        if (users[socket.userNum].strangers[i] === data){
-          users[socket.userNum].strangers.splice(i,1);
+      for (var i = 0; i < users[socket.userNum].strangers.length; i++) {
+        if (users[socket.userNum].strangers[i] === data) {
+          users[socket.userNum].strangers.splice(i, 1);
           users[socket.userNum].friends.push(data);
           socket.emit('addFriendSuccess', 'stranger', data);
           return;
@@ -98,30 +104,30 @@ io.on('connection', function (socket) {
       }
 
       // 默认添加好友后设置自己为对方的陌生人
-      for (var i=0; i<users.length; i++) {
+      for (var i in users) {
         if (users[i].username === data) {
           users[i].strangers.push(users[socket.userNum].username);
-          io.to(users[i].id).emit('addStrangerSuccess',users[socket.userNum].username);
+          io.to(users[i].id).emit('addStrangerSuccess', users[socket.userNum].username);
           break;
         }
       }
 
       // 添加好友成功
       users[socket.userNum].friends.push(data);
-      socket.emit('addFriendSuccess',data);
+      socket.emit('addFriendSuccess', data);
     } else {
       socket.emit('addFriendSuccess', 'none');
     }
   })
 
-  socket.on('addGroup', function(data, user){
+  socket.on('addGroup', function (data, user) {
     // 将用户加入房间名单中
     if (!roomInfo[data]) {
       roomInfo[data] = []
     }
 
-    users[socket.userNum].groups.map((v)=>{
-      if (v === data){
+    users[socket.userNum].groups.map((v) => {
+      if (v === data) {
         socket.emit('addGroupSuccess', 'none');
         return;
       }
@@ -130,36 +136,37 @@ io.on('connection', function (socket) {
     roomInfo[data].push(user);
     // 加入房间
     socket.join(data)
-    
+
     if (users[socket.userNum].username === user.username) {
       users[socket.userNum].groups.push(data);
     }
 
     // 添加群组成功
-    socket.emit('addGroupSuccess', data, users);
+    socket.emit('addGroupSuccess', data);
   })
 
-  socket.on('joinGroup', function(data, user){
+  socket.on('joinGroup', function (data, user) {
     // 将用户加入房间名单中
     roomInfo[data].push(user.username);
     // 在线人数
     // usersNum++;
     // 加入房间
     socket.join(data)
-    
-    for (var i=0; i<users.length; i++) {
+
+    for (var i in users) {
       if (users[i].username === user.username) {
         users[i].groups.push(data);
         break;
       }
     }
+
     var postData = {
       user: user.username,
       groupName: data,
       //len: usersNum,
     }
     // 通知房间内人员
-    io.to(data).emit('online',postData);
+    io.to(data).emit('online', postData);
   })
 
   socket.on('disconnect', function () {
@@ -186,12 +193,20 @@ io.on('connection', function (socket) {
   });
 
   socket.on('chat message', function (data) {
-    //console.log(data.username +":" + data.message);
-    for (let _user of users) {
-      if (_user.username === data.username) {
-        _user.message.push(data.message);
-        //信息存储之后触发receiveMessage将信息发给所有浏览器  
-        io.emit('receiveMessage', data);
+    // 发送方
+    for (var i in users) {
+      if (users[i].username === data.username) {
+        users[i].messages.push(data.message);
+        //信息存储之后触发sendMessage将信息发给发送方
+        io.to(users[i].id).emit('sendMessage', data);
+        break;
+      }
+    }
+    // 接收方
+    for (var i in users) {
+      if (users[i].username === data.touser) {
+        //getMessage将信息发给接收方
+        io.to(users[i].id).emit('getMessage', data, users[i]);
         break;
       }
     }
